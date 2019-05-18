5373a4d2267d6bc79c0052ac62728a3b81c8cf06
gregorzadnik <gregor.zadnik@gmail.com>
2019-05-18 21:35:25 +0200

    Host can read files from "message" directory, scrollView added, follower to be done

--- Android/app/src/main/java/com/example/myapplication/Activities/HostActivity.java
+++ Android/app/src/main/java/com/example/myapplication/Activities/HostActivity.java
@@ -5,32 +5,72 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HostActivity extends AppCompatActivity {
 
     //naredi povezavo s FireBase
-    String imeNaprave = getDeviceName();
-    FirebaseDatabase db = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = db.getReference(imeNaprave);
-
+    String imeNaprave;
+    String pot;
+    FirebaseDatabase db;
+    ArrayList<String> podatki;
     //ko se nalozi screen hosta, se izvede funkcija, ki poslje firebase-u ime trenutne naprave
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
-        sendMessage("Testis");
+        imeNaprave = getDeviceName();
+        pot = "message/";
+        //izpisemo ime naprave
+        TextView ime = (TextView)findViewById(R.id.imeNaprave);
+        ime.setText(imeNaprave);
+
+        podatki = new ArrayList<String>();
+        //sendMessage(this.imeNaprave, "Host");
+        Log.i(null, "Testis1234");
+
+        //zacnemo proces branja podatkov iz baze
+        db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference(pot);
+        // dodamo listener, ki poslusa na mapi ime/
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                podatki = (ArrayList<String>) dataSnapshot.getValue();
+                Log.i("MyApp", "prebrali snapshot: ");
+                Log.i("MyApp", podatki.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        };
+        ref.addValueEventListener(listener);
+
+        //izpisemo vse elemente v podatkih
+
+
     }
 
+    //spremeni activity nazaj na izbiro med Host in Follower
     public void goToMainActivity(View view) {
         startActivity(new Intent(HostActivity.this, MainActivity.class));
     }
 
-    public static String getDeviceName() {
+    //vrne ime naprave/telefona
+    public String getDeviceName() {
         String manufacturer = Build.MANUFACTURER;
         String model = Build.MODEL;
         if (model.startsWith(manufacturer)) {
@@ -38,12 +78,14 @@ public class HostActivity extends AppCompatActivity {
         }
         return capitalize(manufacturer) + " " + model;
     }
-
-    public void sendMessage(String str) {
+    //nastavi value na poti pot na str
+    public void sendMessage(String pot, String str) {
+        //reference je pot na bazi
+        DatabaseReference myRef = db.getReference(pot);
         myRef.setValue(str);
     }
 
-    private static String capitalize(String str) {
+    private String capitalize(String str) {
         if (TextUtils.isEmpty(str)) {
             return str;
         }
@@ -64,6 +106,14 @@ public class HostActivity extends AppCompatActivity {
 
         return phrase.toString();
     }
+    //razred, v katerega shranimo podatke
+    public class Podatki{
+        String[] imena;
+        int stevilo;
 
-
+        public Podatki(String[] imena, int stevilo){
+            this.imena = imena;
+            this.stevilo = stevilo;
+        }
+    }
 }
