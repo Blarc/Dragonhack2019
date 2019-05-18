ed6490efd34b29a04396572c7bb287c056f6cfbf
Blarc <jakob.malezic@gmail.com>
2019-05-18 14:23:40 +0200

    "added Firebase Database dependencies"

--- Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = db.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-
+        myRef.setValue("Hello, World!");
     }
 }
